289382308844d5a3bc8d27a448de3669290b0435
snehasissanyal2 <snehasissanyal2@gmail.com>
2025-06-04 01:49:33 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,8 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=int(input('enter the first number:'))
+... b=int(input('enter the second number:'))
+... print('assition:',a+b)
+... print('substraction:',a-b)
+... print('Multiplication:',a*b)
+... print('Division:',a/b)
